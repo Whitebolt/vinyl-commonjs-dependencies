81b6a10c6ba3262111ec91413f81550a552b09eb
Stephen <stephen@whitebolt.net>
2017-10-24 17:16:36 +0100

    Fix bug in older nodes

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const vinylCjsDeps = {};
 
 vinylCjsDeps.src = require('./build/src').bind(vinylCjsDeps);
 
-vinylCjsDeps.dest = (...params)=>{
-		if (vinylCjsDeps.gulp) return vinylCjsDeps.gulp.dest.bind(vinylCjsDeps.gulp)(...params);
+vinylCjsDeps.dest = ()=>{
+		if (vinylCjsDeps.gulp) return vinylCjsDeps.gulp.dest.apply(vinylCjsDeps.gulp, arguments);
 		throw new SyntaxError(`No dest() available because gulp has not been set.`);
 };
 
